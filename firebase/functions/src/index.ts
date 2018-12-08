35f7a1d23bd0ce24dd078aa2dada47d449d67e00
James Ah Yong <retrocraft314@gmail.com>
2018-12-08 17:33:41 -0500

    updateBed function

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,10 +1,34 @@
-import * as functions from 'firebase-functions';
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
 
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+const hospitals = db.collection("hospitals");
 
+export const updateBed = functions.https.onRequest((req, res) => {
+  const { hospitalId, occupied } = req.query;
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+  if (!hospitalId || !occupied) {
+    res.status(400).send("Missing parameters");
+    return;
+  }
+
+  const hospital = hospitals.doc(hospitalId);
+  db.runTransaction(t => {
+    return hospital.get().then(doc => {
+      // Add one person to beds
+      if (!hospital.id) {
+        throw new Error("Hospital does not exist");
+      }
+      const newBeds = +doc.data().beds + (!!+occupied ? -1 : 1);
+      t.update(hospital, { beds: newBeds });
+      return newBeds;
+    });
+  })
+    .then(newBeds => {
+      res.send("New capacity: " + newBeds);
+    })
+    .catch(err => {
+      res.send("Transaction failure: " + err);
+    });
 });
